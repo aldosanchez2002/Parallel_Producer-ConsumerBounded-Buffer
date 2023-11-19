165cb638e714bb141175dfa7e1d8efc5d13cda1b
Aldo Sanchez <52171931+aldosanchez2002@users.noreply.github.com>
2023-11-19 16:43:56 -0500

    fixed formatting

--- prodcons.c
+++ prodcons.c
@@ -12,15 +12,15 @@
 
 // use global variables for shared buffer state
 static char *buff;    // the buffer
-static int  N;        // total capacity
-static int  size;     // current num elements
-static int  next_in;  // next insertion index
-static int  next_out; // next remove index
-static int  num_items; // number of items each thread should produce or consume
+static int N;         // total capacity
+static int size;      // current num elements
+static int next_in;   // next insertion index
+static int next_out;  // next remove index
+static int num_items; // number of items each thread should produce or consume
 
-//Make Lock
+// Make Lock
 pthread_mutex_t queueLock;
-//Make Condition Variables
+// Make Condition Variables
 pthread_cond_t queueNotFull;
 pthread_cond_t queueNotEmpty;
 
@@ -32,16 +32,18 @@ void *consumer(void *arg);
 static void *my_malloc(size_t size);
 static void add_to_queue(char item);
 static char remove_from_queue(void);
-static void print_buffer(void);      // for debugging
+static void print_buffer(void); // for debugging
 
 /***************************************************************/
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
   pthread_t *ptids, *ctids;
   int i, num_prods, num_cons;
   // add any locals you need
 
-  if(argc != 4) {
+  if (argc != 4)
+  {
     printf("usage: ./prodcons n_prods_cons n_items buff_size\n");
     exit(1);
   }
@@ -49,80 +51,88 @@ int main(int argc, char **argv) {
   num_items = atoi(argv[2]);
   N = atoi(argv[3]);
   printf("%d Producers & %d Consumers, each producing %d items, buff size %d\n",
-      num_prods, num_cons, num_items, N);
+         num_prods, num_cons, num_items, N);
 
-  ptids = (pthread_t *)my_malloc(sizeof(pthread_t)*num_prods);
-  ctids = (pthread_t *)my_malloc(sizeof(pthread_t)*num_cons);
+  ptids = (pthread_t *)my_malloc(sizeof(pthread_t) * num_prods);
+  ctids = (pthread_t *)my_malloc(sizeof(pthread_t) * num_cons);
 
-  buff = (char *)my_malloc(sizeof(char)*N);
+  buff = (char *)my_malloc(sizeof(char) * N);
   size = 0;
   next_in = 0;
   next_out = 0;
 
-  //initialize lock
+  // initialize lock
   pthread_mutex_init(&queueLock, NULL);
 
-  //initialize condition variables
+  // initialize condition variables
   pthread_cond_init(&queueNotFull, NULL);
   pthread_cond_init(&queueNotEmpty, NULL);
 
-  //        initialize any synchronization primitives prior to this
-  for (i=0; i < num_prods; i++) {
-    pthread_create(&ptids[i], 0, producer, (void*)&num_items);
-
+  // initialize any synchronization primitives prior to this
+  for (i = 0; i < num_prods; i++)
+  {
+    pthread_create(&ptids[i], 0, producer, (void *)&num_items);
   }
 
-  for (i=0; i < num_cons; i++) {
-    
-pthread_create(&ctids[i], 0, consumer, (void*)&num_items);
+  for (i = 0; i < num_cons; i++)
+  {
+    pthread_create(&ctids[i], 0, consumer, (void *)&num_items);
   }
 
   // wait for threads to exit
-  for (i=0; i < num_prods; i++) {
+  for (i = 0; i < num_prods; i++)
+  {
     pthread_join(ptids[i], 0);
   }
-  for (i=0; i < num_cons; i++) {
+  for (i = 0; i < num_cons; i++)
+  {
     pthread_join(ctids[i], 0);
   }
 
-  free ((char *)buff);
-  free ((pthread_t *)ptids);
-  free ((pthread_t *)ctids);
+  free((char *)buff);
+  free((pthread_t *)ptids);
+  free((pthread_t *)ctids);
   pthread_mutex_destroy(&queueLock);
   pthread_cond_destroy(&queueNotFull);
   pthread_cond_destroy(&queueNotEmpty);
-  //print size of buffer
+  // print size of buffer
   printf("%d\n", size);
   exit(0);
 }
 /****************************************************************/
-void *producer(void *arg){
-  int num_items = *(int *) arg;
+void *producer(void *arg)
+{
+  int num_items = *(int *)arg;
   // printf("Producer thread %ld\n", pthread_self());
-  for (int i = 0; i < num_items; i++) {
+  for (int i = 0; i < num_items; i++)
+  {
     // Acquire queue lock
     pthread_mutex_lock(&queueLock);
-    // while queue is full, wait on (queue not-full condition, queue_lock)
-    if (size == N) {
+    // If queue is full, wait on (queue not-full condition, queue_lock)
+    if (size == N)
+    {
       pthread_cond_wait(&queueNotFull, &queueLock);
     }
-      add_to_queue('x');
+    add_to_queue('x');
     pthread_cond_signal(&queueNotEmpty);
     pthread_mutex_unlock(&queueLock);
   }
   return NULL;
 }
 /****************************************************************/
-void *consumer(void *arg){
-  int num_items = *(int *) arg;
+void *consumer(void *arg)
+{
+  int num_items = *(int *)arg;
   // printf("Consumer thread %ld\n", pthread_self());
-  for (int i = 0; i < num_items; i++) {
+  for (int i = 0; i < num_items; i++)
+  {
     pthread_mutex_lock(&queueLock);
-    // While queue is empty, wait on (queue not-empty condition, queue_lock)
-    if (size == 0) {
+    // If queue is empty, wait on (queue not-empty condition, queue_lock)
+    if (size == 0)
+    {
       pthread_cond_wait(&queueNotEmpty, &queueLock);
     }
-      remove_from_queue();
+    remove_from_queue();
     // Signal (queue not-full condition)
     pthread_cond_signal(&queueNotFull);
     // Release queue lock
@@ -137,7 +147,8 @@ void *consumer(void *arg){
 // space to add, the caller is responsible for that
 // item: the value to add
 
-static void add_to_queue(char item) {
+static void add_to_queue(char item)
+{
 
   buff[next_in] = item;
   next_in = (next_in + 1) % N;
@@ -150,7 +161,8 @@ static void add_to_queue(char item) {
 // in the queue to remove, the caller is responsible for that
 //   returns: the next item to remove
 //
-static char remove_from_queue() {
+static char remove_from_queue()
+{
   char item;
   item = buff[next_out];
   next_out = (next_out + 1) % N;
@@ -159,24 +171,27 @@ static char remove_from_queue() {
 }
 /****************************************************************/
 // print out the contents of the buffer
-static void print_buffer() {
+static void print_buffer()
+{
   int i, index;
   printf("Buffer size %d ************************\n", size);
-  for(i=0; i < size; i++) {
-     index = (next_out + i) % N;
-     printf("%2d:%2d ", index, buff[index]);
+  for (i = 0; i < size; i++)
+  {
+    index = (next_out + i) % N;
+    printf("%2d:%2d ", index, buff[index]);
   }
   printf("\n next in = %d  next out %d\n", next_in, next_out);
   printf("***************************************\n");
-
 }
 /****************************************************************/
 // a wrapper function around malloc that calls perror and exit
 // on error
-static void *my_malloc(size_t size){
+static void *my_malloc(size_t size)
+{
   void *ret;
   ret = malloc(size);
-  if(!ret) {
+  if (!ret)
+  {
     perror("malloc array error");
     exit(1);
   }
